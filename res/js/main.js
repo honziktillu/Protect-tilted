761ba5c6ea19f0f44d5e37f861bd2b5bf8398e7d
honziktillu <till@spsmb.cz>
2024-01-12 09:13:54 +0100

    added movement and img for enemies

--- res/js/main.js
+++ res/js/main.js
@@ -1,7 +1,8 @@
 import { Enemy } from "./ui/entities.js";
-import { Background } from "./ui/basic-ui.js"
+import { Background } from "./ui/basic-ui.js";
 
-const foo = new Enemy("Tomas", 50, 1);
+const battleBus = new Enemy("BattleBus", 50, 1, 0, 50, 300);
+const fnKid = new Enemy("fnKid", 50, 1, 2, 50, 100);
 const background = new Background();
 
 const canvas = document.getElementById("canvas");
@@ -18,19 +19,24 @@ const gameLoop = () => {
   fps();
   //gameLoop znovu
   window.requestAnimationFrame(gameLoop);
-}
+};
 
 const clear = () => {
   canvas.width = 1280;
   canvas.height = 720;
   background.draw(ctx);
-}
+};
 
-
-const update = () => {}
-const render = () => {}
-const fps = () => {}
+const update = () => {
+  battleBus.update();
+  fnKid.update();
+};
+const render = () => {
+  battleBus.draw(ctx);
+  fnKid.draw(ctx);
+};
+const fps = () => {};
 
 window.onload = () => {
   window.requestAnimationFrame(gameLoop);
-}
+};
--- res/js/ui/entities.js
+++ res/js/ui/entities.js
@@ -3,17 +3,64 @@
 // šablona - třída (class)
 // třída (class) má vždy první písmeno velké
 export class Enemy {
+  //constructor - metoda ktera se vola pri vytvoreni objektu
+  constructor(name, hp, dmg, type, x, y) {
+    //this - ukazuje na vzniklou instanci - kopii od tridy
+    this.name = name;
+    this.hp = hp;
+    this.dmg = dmg;
+    this.img = new Image();
+    this.setType(type);
+    this.img.src = this.path;
+    this.position = {
+      x: x,
+      y: y,
+    };
+    this.ratio = 0.5;
+    this.size = {
+      width: 200 * this.ratio,
+      height: 200 * this.ratio,
+    };
+    this.velocity = {
+      x: 2,
+      y: 0,
+    };
+  }
 
-    //constructor - metoda ktera se vola pri vytvoreni objektu
-    constructor(name, hp, dmg) {
-        //this - ukazuje na vzniklou instanci - kopii od tridy
-        this.name = name;
-        this.hp = hp;
-        this.dmg = dmg;
+  draw(ctx) {
+    ctx.drawImage(
+      this.img,
+      this.position.x,
+      this.position.y,
+      this.size.width,
+      this.size.height
+    );
+  }
+
+  setType(type) {
+    const paths = [
+      "./res/img/enemies/battle_bus.png",
+      "./res/img/enemies/BOSS.png",
+      "./res/img/enemies/fnkid.png",
+      "./res/img/enemies/kevin.png",
+      "./res/img/enemies/mecha.png",
+    ];
+    this.path = paths[type];
+  }
+
+  update() {
+    this.move();
+  }
+
+  move() {
+    this.position.x += this.velocity.x;
+    if (this.position.x >= 550) {
+      this.velocity.x *= -1;
     }
-
+    if (this.position.x <= 0) {
+      this.velocity.x *= -1;
+    }
+  }
 }
 
 //const myEnemy = new Enemy("Enemy 1", 100, 12); //vytvoreni kopie od šablony - objekt(instance)
-
-
